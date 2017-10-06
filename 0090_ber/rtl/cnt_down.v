795b5835b0c90c0516d49e0a00133469e5158944
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2017-10-06 17:27:18 +0900

    First checkin.

---
+++ 0090_ber/rtl/cnt_down.v
@@ -0,0 +1,20 @@
+module cnt_down #(parameter BW = 'd8) (
+ input               RSTX,
+ input               CLK,
+ input               DEC,
+ input               LOAD,
+ input      [BW-1:0] VAL,
+ output              CNT0,
+ output reg [BW-1:0] CNT,
+ output     [BW-1:0] CNT_NEXT
+);
+
+assign CNT_NEXT = LOAD ? VAL
+                : DEC  ? CNT + {BW{1'b1}};
+                :        CNT
+always @(posedge CLK or negedge RSTX)
+  if (!RSTX) CNT <= {BW{1'b0}};
+  else       CNT <= CNT_NEXT;
+assign CNT0 = CNT == {BW{1'b0}};
+
+endmodule
