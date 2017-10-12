5550f7c9d0b62be96520d37c61789d347a7988a3
yammouch <yammouch@gmail.com>
2017-10-12 18:03:03 +0900

    Debug output for BER.

--- 0090_ber/sim/scenario/t020.v
+++ 0090_ber/sim/scenario/t020.v
@@ -36,6 +36,31 @@ begin
 end
 endtask
 
+function [3:0] decode_7seg(input [6:0] seg);
+  case (seg)
+  7'b0111111: decode_7seg = 4'd0;
+  7'b0000110: decode_7seg = 4'd1;
+  7'b1011011: decode_7seg = 4'd2;
+  7'b1001111: decode_7seg = 4'd3;
+  7'b1100110: decode_7seg = 4'd4;
+  7'b1101101: decode_7seg = 4'd5;
+  7'b0111111: decode_7seg = 4'd6;
+  7'b0100111: decode_7seg = 4'd7;
+  7'b1111111: decode_7seg = 4'd8;
+  7'b1101111: decode_7seg = 4'd9;
+  default   : decode_7seg = 4'dx;
+  endcase
+endfunction
+
+wire [3:0] dig_m1   = decode_7seg(dut.i_handle_7seg.dig_m  [13: 7]);
+wire [3:0] dig_m0   = decode_7seg(dut.i_handle_7seg.dig_m  [ 6: 0]);
+wire [3:0] dig_s1   = decode_7seg(dut.i_handle_7seg.dig_s  [13: 7]);
+wire [3:0] dig_s0   = decode_7seg(dut.i_handle_7seg.dig_s  [ 6: 0]);
+wire [3:0] dig_ber3 = decode_7seg(dut.i_handle_7seg.dig_ber[27:21]);
+wire [3:0] dig_ber2 = decode_7seg(dut.i_handle_7seg.dig_ber[20:14]);
+wire [3:0] dig_ber1 = decode_7seg(dut.i_handle_7seg.dig_ber[13: 7]);
+wire [3:0] dig_ber0 = decode_7seg(dut.i_handle_7seg.dig_ber[ 6: 0]);
+
 task test_main;
 reg [31:0] f_hdl;
 begin
