2f201f203a5f77b6aca0fea1c3b9015ae81e5f5d
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2017-10-30 18:21:10 +0900

    Updated.

--- 0130_8bit/rtl/lfsr32x2.v
+++ 0130_8bit/rtl/lfsr32x2.v
@@ -1,10 +1,10 @@
 module lfsr32x2 (
- input  [15:0] DIN,
- output [15:0] DOUT);
+ input  [7:0] DIN,
+ output [7:0] DOUT);
 
-assign DOUT = DIN == 16'd0
-            ? 16'd1
-            :   { DIN[14:0], 1'b0 }
-              ^ ( DIN[15] ? 16'h3401 : 16'd0 );
+assign DOUT = DIN == 8'd0
+            ? 8'd1
+            :   { DIN[6:0], 1'b0 }
+              ^ ( DIN[7] ? 8'h71 : 8'd0 );
 
 endmodule
