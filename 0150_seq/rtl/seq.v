adf0cfa168b13746be8df9010055d904f4bfc2c7
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2017-10-31 20:50:09 +0900

    First checkin.

---
+++ 0150_seq/rtl/seq.v
@@ -0,0 +1,48 @@
+module seq #(
+ parameter BW_SEQ        = 3'd4,
+           SEQ_CNT       = 3'd7,
+           BW_SEQ_CNT    = 2'd3,
+           BW_BW_SEQ_CNT = 2'd2,
+           RV            = {BW_SEQ{1'b0}},
+           BW_TIMEOUT    = 2'd2
+) (
+ input                                            RSTX,
+ input                                            CLK,
+ input                                            CLR,
+ input      [(BW_SEQ+BW_TIMEOUT)*(SEQ_CNT+1)-1:0] PTN, 
+ output reg [                         BW_SEQ-1:0] SEQ
+);
+
+wire [BW_BW_SEQ_CNT-1:0] cnt_next, cnt;
+wire                     cnt0;
+
+cnt_down #(.BW(BW_BW_SEQ_CNT)) i_cnt_down_p (
+ .RSTX     (RSTX),
+ .CLK      (CLK),
+ .LOAD     (CLR),
+ .VAL      (BW_SEQ_CNT),
+ .DEC      (cnt0),
+ .CNT_NEXT (cnt_next),
+ .CNT      (cnt),
+ .CNT0     ()
+);
+
+reg [BW_TIMEOUT-1:0] timeout;
+always @* timeout = PTN >> ((BW_TIMEOUT+BW_SEQ)*cnt_next);
+
+cnt_down #(.BW(BW_TIMEOUT)) i_cnt_down_s (
+ .RSTX     (RSTX),
+ .CLK      (CLK),
+ .LOAD     (CLR),
+ .VAL      (timeout),
+ .DEC      (1'b1),
+ .CNT_NEXT (),
+ .CNT      (),
+ .CNT0     (cnt0)
+);
+
+always @(posedge CLK or negedge RSTX)
+  if (!RSTX) SEQ <= RV;
+  else       SEQ <= PTN >> ((BW_TIMEOUT+BW_SEQ)*cnt+BW_TIMEOUT);
+
+endmodule
---
+++ 0150_seq/rtl/synth.v
@@ -0,0 +1,28 @@
+module synth (
+ input        RSTX,
+ input        CLK,
+ input        CLR,
+ output [5:0] SEQ
+);
+
+seq #(
+ .BW_SEQ        (3'd6),
+ .SEQ_CNT       (3'd5),
+ .BW_SEQ_CNT    (2'd3),
+ .BW_BW_SEQ_CNT (2'd2),
+ .RV            (6'b000001),
+ .BW_TIMEOUT    (2'd3)
+) i_seq (
+ .RSTX (RSTX),
+ .CLK  (CLK),
+ .CLR  (CLR),
+ .PTN  ({6'b000001, 3'd0,
+         6'b000010, 3'd1,
+         6'b000100, 3'd2,
+         6'b001000, 3'd3,
+         6'b010000, 3'd4,
+         6'b100000, 3'd5}),
+ .SEQ  (SEQ)
+);
+
+endmodule
